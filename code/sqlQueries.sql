24701dc6cd5f8903aa72eef5a0bfd88b6daa3964
gydis <egor.eremin@aalto.fi>
2022-05-23 21:40:09 +0300

    Fixed queries' attribute names

--- code/sqlQueries.sql
+++ code/sqlQueries.sql
@@ -1,18 +1,18 @@
 -- Query 1
-SELECT staff.ssn, staff.name, staff.phone, staff.role, staff.vaccstatus, vaccinationshift.hospital
-FROM staff, vaccination_event, vaccinationshift
-WHERE vaccination_event.date = '2021-05-10' AND vaccinationshift.hospital = vaccination_event.location AND vaccinationshift.weekday = 'Monday' AND vaccinationshift.worker = staff.ssn;
+SELECT staff.ssn, staff.name, staff.phone, staff.role, staff.vacc_status, vaccination_shift.hospital
+FROM staff, vaccination_event, vaccination_shift
+WHERE vaccination_event.date = '2021-05-10' AND vaccination_shift.hospital = vaccination_event.hospital AND vaccination_shift.weekday = 'Monday' AND vaccination_shift.worker = staff.ssn;
 
--- Query 2
-SELECT staff.ssn, staff.name, staff.birthday, staff.phone, staff.role, staff.vaccstatus
-FROM staff, vaccinationshift
-WHERE staff.ssn = vaccinationshift.worker AND staff.role = 'doctor' AND  staff.ssn IN (SELECT staff.ssn FROM staff, vaccinationshift WHERE vaccinationshift.weekday = 'Wednesday' AND vaccinationshift.worker = staff.ssn); 
+-- Query 2 (Outputs identical rows without DISTINCT on staff.ssn)
+SELECT DISTINCT staff.ssn, staff.name, staff.birthday, staff.phone, staff.role, staff.vacc_status
+FROM staff, vaccination_shift
+WHERE staff.ssn = vaccination_shift.worker AND staff.role = 'doctor' AND  staff.ssn IN (SELECT staff.ssn FROM staff, vaccination_shift WHERE vaccination_shift.weekday = 'Wednesday' AND vaccination_shift.worker = staff.ssn); 
 
 -- Query 6
-SELECT hospital.name AS hospital_name, SUM(batch.numberofvacc) AS total_vaccines, 
-SUM(case when vacctype='V01' then batch.numberofvacc else 0 end) as V01_amount,
-SUM(case when vacctype='V02' then batch.numberofvacc else 0 end) as V02_amount,
-SUM(case when vacctype='V03' then batch.numberofvacc else 0 end) as V03_amount
+SELECT hospital.name AS hospital_name, SUM(batch.num_of_vacc) AS total_vaccines, 
+SUM(case when vaccine_type='V01' then batch.num_of_vacc else 0 end) as "V01_amount",
+SUM(case when vaccine_type='V02' then batch.num_of_vacc else 0 end) as "V02_amount",
+SUM(case when vaccine_type='V03' then batch.num_of_vacc else 0 end) as "V03_amount"
 FROM batch, hospital
-WHERE hospital.name = batch.location
+WHERE hospital.name = batch.hospital
 GROUP BY hospital.name;
