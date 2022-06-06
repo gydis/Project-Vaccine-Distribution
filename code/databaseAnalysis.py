02ff710e234cc6f81ffe054d02530aedffe8af03
BehramUlukir <behramulukir@gmail.com>
2022-06-06 17:00:09 +0300

    More editing about req 9

--- code/databaseAnalysis.py
+++ code/databaseAnalysis.py
@@ -353,7 +353,7 @@ def main():
         dfR9_full.plot(ax=q9_plot)#.get_figure().savefig(DATADIR + '/img/part3_req9.png')
 
         #Uncomment this to see plot, however, doing so will prevent you seeing results of requirement 10
-        plt.show()
+        #plt.show()
 
         #Part 3, Requirement 10
         query_10 = """
